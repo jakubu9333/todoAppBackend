d5db3795574fb2a91935ce86f91ad9a10a9a30bc
jakubu9333 <jkbuhlarik3@gmail.com>
2024-03-04 23:40:07 +0100

    add todo routes

--- src/main/kotlin/com/jakubu9333/plugins/Routing.kt
+++ src/main/kotlin/com/jakubu9333/plugins/Routing.kt
@@ -1,11 +1,20 @@
 package com.jakubu9333.plugins
 
+import com.jakubu9333.repositories.InMemoryTodoRepository
+import com.jakubu9333.routes.apiDeleteTodo
+import com.jakubu9333.routes.apiGetAllTodo
+import com.jakubu9333.routes.apiPostTodo
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
+    val inMemoryTodoRepository = InMemoryTodoRepository()
     routing {
+        apiPostTodo(inMemoryTodoRepository)
+        apiDeleteTodo(inMemoryTodoRepository)
+        apiGetAllTodo(inMemoryTodoRepository)
+
         get("/") {
             call.respondText("Hello World!")
         }
---
+++ src/main/kotlin/com/jakubu9333/routes/ApiRoutes.kt
@@ -0,0 +1,42 @@
+package com.jakubu9333.routes
+
+import com.jakubu9333.repositories.TodoItem
+import com.jakubu9333.repositories.TodoRepository
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+
+fun Route.apiPostTodo(todoRepository: TodoRepository) {
+    route("/api/todo"){
+        post {
+            val todo = call.receive<TodoItem>()
+            val id = todoRepository.add(todo)
+            call.respond(HttpStatusCode.Created,id)
+        }
+
+    }
+}
+
+fun Route.apiGetAllTodo(todoRepository: TodoRepository) {
+    get("/api/todo") {
+
+        val todos = todoRepository.getAll()
+        call.respond(HttpStatusCode.OK, todos)
+    }
+}
+
+fun Route.apiDeleteTodo(todoRepository: TodoRepository) {
+    delete ("/api/todo/{id}") {
+        val id = call.parameters["id"]?.toInt()
+        if (id==null){
+            call.respond(HttpStatusCode.BadRequest,"no id")
+            return@delete
+        }
+
+        todoRepository.delete(id)
+        call.respond(HttpStatusCode.NoContent)
+    }
+}
